632274211984c6d17e454abf1c4c92c971900b18
Abdul_Hanan <abdulhanan3279@gmail.com>
2022-12-17 14:08:40 +0500

    hero-section-cards

--- src/pages/home/HeroSection.jsx
+++ src/pages/home/HeroSection.jsx
@@ -7,36 +7,37 @@ import HomeInputGroup from "../../components/HeroInputGroup"
 function HeroSection() {
   return (
     <>
-      <section className='hero'>
-        <div className="container">
-          {/* hero section main text div  */}
-          <div className='hero-section-main '>
-            <h1>17.5 million or more creative assets</h1>
-            <p className='hero-section-main-p1'>SVGs, vector icons, illustrations, 3D graphics, and Lottie animations.</p>
-            <p className='hero-section-main-p2'>that have been carefully selected
-              Every day, or more 7000 new assets are added.<br /> Integrated tools, editors, plugins, and much more.</p>
+      <div className='hero-main-div position-relative'>
+        <section className='hero'>
+          <div className="container">
+            {/* hero section main text div  */}
+            <div className='hero-section-main '>
+              <h1>17.5 million or more creative assets</h1>
+              <p className='hero-section-main-p1'>SVGs, vector icons, illustrations, 3D graphics, and Lottie animations.</p>
+              <p className='hero-section-main-p2'>that have been carefully selected
+                Every day, or more 7000 new assets are added.<br /> Integrated tools, editors, plugins, and much more.</p>
+            </div>
+            {/* hero section main div  end */}
+            {/* form input field */}
+            <div>
+              <form className='hero-section-form'>
+                <HomeInputGroup />
+              </form>
+            </div>
           </div>
-          {/* hero section main div  end */}
-          {/* form input field */}
-          <div>
-            <form className='hero-section-form'>
-              <HomeInputGroup />
-            </form>
-          </div>
-        </div>
 
-      </section>
-      <section className='card-section'>
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-3 col-md-4 col-sm-6"><CardMain title='' text='' bgColor=''/></div>
-            <div className="col-lg-3 col-md-4 col-sm-6"><CardMain title='' text='' bgColor=''/></div>
-            <div className="col-lg-3 col-md-4 col-sm-6"><CardMain title='' text='' bgColor=''/></div>
-            <div className="col-lg-3 col-md-4 col-sm-6"><CardMain title='' text='' bgColor=''/></div>
+        </section>
+        <section className='card-section position-absolu'>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-3 col-md-4 col-sm-6"><CardMain title='3D Illustrations' text='118,300 + 3d Contents in 3D with input file' bgColor='#DAFEEA ' /></div>
+              <div className="col-lg-3 col-md-4 col-sm-6"><CardMain title='Lottie Animations' text='79,000+ Mini Animations, Both Free and Paid' bgColor='#EEF8F9' /></div>
+              <div className="col-lg-3 col-md-4 col-sm-6"><CardMain title='Illustrations' text='152,000+ vector illustration colour editing software' bgColor='#FCF2F2' /></div>
+              <div className="col-lg-3 col-md-4 col-sm-6"><CardMain title='Vector Icons' text='4.9 million plus vector icons, colour editor' bgColor='#FFE5EE' /></div>
+            </div>
           </div>
-        </div>
-      </section>
-
+        </section>
+      </div>
     </>
   )
 }
